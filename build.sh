59be07be64c17c48102624230959a1cf983ac833
sanjaypj035 <sanjaypj035@gmail.com>
2025-07-31 22:58:39 +0530

    Final fix: Use a self-contained build.sh script to install Flutter and deploy

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Define the Flutter SDK version to use
+FLUTTER_VERSION="3.19.0"
+
+# Download and extract the Flutter SDK
+wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz
+tar xf flutter_linux_${FLUTTER_VERSION}-stable.tar.xz
+
+# Add Flutter to the PATH for the rest of the script
+export PATH="$PATH:$(pwd)/flutter/bin"
+
+# Get project dependencies
+flutter pub get
+
+# Build the web application
+flutter build web --release --web-renderer canvaskit --base-href /
+
+# Tell Vercel where the output is located
+cp -r build/web/* .
