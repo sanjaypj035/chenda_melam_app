fd4a4b6ece78975fa2d4dd37e8b5b0a52e40e10b
sanjaypj035 <sanjaypj035@gmail.com>
2025-07-31 23:14:14 +0530

    Fix blank white screen by using a relative base-href in build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # Define the Flutter SDK version to use
 FLUTTER_VERSION="3.19.0"
 
-# Download and extract the Flutter SDK using curl
+# Download and extract the Flutter SDK
 curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz
 tar xf flutter_linux_${FLUTTER_VERSION}-stable.tar.xz
 
@@ -13,8 +13,8 @@ export PATH="$PATH:$(pwd)/flutter/bin"
 # Get project dependencies
 flutter pub get
 
-# Build the web application
-flutter build web --release --web-renderer canvaskit --base-href /
+# Build the web application with a relative base-href
+flutter build web --release --web-renderer canvaskit --base-href ./
 
 # Tell Vercel where the output is located
 cp -r build/web/* .
