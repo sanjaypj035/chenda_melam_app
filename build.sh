4f3dfe1ff39bbac530100454a6b58e7093a7e476
sanjaypj035 <sanjaypj035@gmail.com>
2025-07-31 22:43:42 +0530

    Use @sanchitgoyal/vercel-flutter builder and remove build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Install Flutter dependencies
-flutter pub get
-
-# Build the Flutter web application
-flutter build web --release --web-renderer canvaskit --base-href /
-
-# Copy the built files to the output directory
-cp -r build/web/* .
