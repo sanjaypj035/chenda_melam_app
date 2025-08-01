9bd02c2007f75ecff7561e3c918ab64b97f404c2
sanjaypj035 <sanjaypj035@gmail.com>
2025-08-01 21:41:07 +0530

    Final fix: Added Git config for safe directory to fix dubious ownership error

--- build.sh
+++ build.sh
@@ -10,8 +10,14 @@ tar xf flutter_linux_${FLUTTER_VERSION}-stable.tar.xz
 # Add Flutter to the PATH for the rest of the script
 export PATH="$PATH:$(pwd)/flutter/bin"
 
+# IMPORTANT: Fix the Git dubious ownership error
+git config --global --add safe.directory /vercel/path0/flutter
+
 # Get project dependencies
 flutter pub get
 
 # Build the web application with a relative base-href
-flutter build web --release --web-renderer canvaskit --base-href /
+flutter build web --release --web-renderer canvaskit --base-href ./
+
+# Tell Vercel where the output is located
+cp -r build/web/* .
