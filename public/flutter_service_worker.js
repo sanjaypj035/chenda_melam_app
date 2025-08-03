329e894a16ddbf0424c2739100ff4ff61f5faef3
sanjaypj035 <sanjaypj035@gmail.com>
2025-08-03 19:57:36 +0530

    Deploy static Flutter site to Vercel

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c7c19a277ce74afb237bd3474159ef96
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "f2d377687dce1e93f1c518ded8c0a937",
+"flutter_bootstrap.js": "6b9f01cead2c6758b6339f5a31be6d56",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
