e800bdc4e8a016396eef84500e81cde24c55aa4e
sanjaypj035 <sanjaypj035@gmail.com>
2025-08-03 19:24:09 +0530

    Update built Flutter web files

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c7c19a277ce74afb237bd3474159ef96
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "e04504313b083983ebeea377dc2a2028",
+"flutter_bootstrap.js": "bc161f24af7d2db99366d9c1b79ced2b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
