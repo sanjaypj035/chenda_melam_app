c20802b5479f05320995c7a84bb9ae82a968d76b
sanjaypj035 <sanjaypj035@gmail.com>
2025-08-03 13:30:08 +0530

    Fix: Force flutter pub get using downloaded Flutter (Dart 3.8)

--- build.sh
+++ build.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# Use Flutter SDK version 3.22.2 (Dart 3.8.0)
+# Use Flutter SDK version 3.22.2 (which includes Dart 3.8.0)
 FLUTTER_VERSION="3.22.2"
 
-# Download and extract Flutter SDK
+# Download Flutter SDK
 curl -O https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz
 tar xf flutter_linux_${FLUTTER_VERSION}-stable.tar.xz
 
-# Git safe config
+# Git safety config
 git config --global --add safe.directory /vercel/path0/flutter
 
-# Use ONLY local Flutter binary
+# Use ONLY local flutter for all commands
 ./flutter/bin/flutter --version
+./flutter/bin/flutter config --no-analytics
 ./flutter/bin/flutter pub get
 ./flutter/bin/flutter build web --release --web-renderer canvaskit --base-href "/"
